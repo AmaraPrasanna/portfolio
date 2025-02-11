7ab9c4407a48e756da0b915b8fae8c830f644e76
Parth Shiroya <parthshiroya2001@gmail.com>
2025-02-10 17:18:38 -0800

    spacings

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,7 +10,7 @@ export const EXPERIENCES = [
   {
     year: "January 2025 - Present",
     role: "Data Analytics Intern",
-    company: "California Air Resources Board (CARB), Sacramento",
+    company: " California Air Resources Board (CARB), Sacramento",
     description:`
     At CARB, I am responsible for developing and maintaining Power BI dashboards that provide real-time insights into air quality data. By creating interactive reports, I enable users to explore and analyze air pollution trends, supporting data-driven decision-making. Additionally, I utilize Python for data preprocessing and transformation, optimizing data workflows for enhanced efficiency and accuracy.`,
 
@@ -20,7 +20,7 @@ export const EXPERIENCES = [
   {
     year: "May 2024 - August 2024",
     role: "Software Developer Intern",
-    company: "Yardi Systems",
+    company: " Yardi Systems",
     description:`
      During my internship at Yardi Systems, I contributed to building interactive dashboards using Angular and TypeScript, ensuring a seamless user experience with real-time data updates. I worked extensively with SQL Server, optimizing data retrieval and manipulation through advanced queries and data-binding techniques. My role also involved developing robust C#/.NET backend services to facilitate smooth frontend-backend communication. Furthermore, I designed and implemented RESTful APIs using .NET Core, integrating authentication, authorization, and error handling to ensure secure and scalable system interactions.`,
 
@@ -30,7 +30,7 @@ export const EXPERIENCES = [
   {
     year: "January 2024 - Present",
     role: "Web Developer Student Assistant",
-    company: "CSU Sacramento",
+    company: " CSU Sacramento",
     description: `
     As a Web Developer Student Assistant at California State University, Sacramento, I focus on developing and maintaining responsive web applications for the university’s technology department. My work involves using HTML5, CSS3, JavaScript (ES6), and PHP to enhance online platforms and improve user experience. I implement client-side validation and efficient form handling to optimize usability. Additionally, I manage version control through Git, collaborating on code reviews and merge requests. Apart from development, I provide Tier 1 & Tier 2 technical support, troubleshooting hardware, software, and network-related issues for students, faculty, and staff.`,
      technologies: ["HTML", "CSS", "JavaScript", "PHP"],
@@ -38,7 +38,7 @@ export const EXPERIENCES = [
   {
     year: "January 2023 - May 2023",
     role: "Web Developer Intern",
-    company: "Tops Technologies",
+    company: " Tops Technologies",
     description: `
     At Tops Technologies, I was part of a team that developed a comprehensive online household services ordering platform using Java, Spring MVC, and MySQL. I played a crucial role in designing and implementing three web applications: a customer portal, a service provider portal, and an admin portal. The platform was built using a responsive design approach to ensure accessibility across different devices. Additionally, I integrated key features such as user registration, service ordering, and payment processing via the Razorpay API, enhancing functionality and user experience.`,
     technologies: ["JAVA", "MySQL", "RazorPay", "Bootstrap"],
